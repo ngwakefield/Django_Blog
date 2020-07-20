c6e2c6fcbc6a0448a316696ce8d9536b58c6f49a
Nick Wakefield <n.g.wakefield@gmail.com>
2020-07-20 02:22:31 -0500

    Modify website to use Post objects from database

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author' : 'nwakefi',
-        'title' : 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted' : '2018-08-27'
-    },
-    {
-        'author' : 'nwakefi',
-        'title' : 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted' : '2018-08-28'
-    },
-    {
-        'author' : 'testuser',
-        'title' : 'Blog Post 3',
-        'content': 'New post content',
-        'date_posted' : '2018-09-01'
-    }
-]
+from .models import Post
 
 
 def home(request):
-    context = {'posts':posts}
+    context = {'posts':Post.objects.all()}
     return render(request, 'blog/home.html', context)
 
 def about(request):
