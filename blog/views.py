70f3e8f66f5ae0d773f201bedb924958c776f1a7
Nick Wakefield <n.g.wakefield@gmail.com>
2020-08-15 22:27:17 -0500

    Moving to class based views

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
 from .models import Post
 
 
@@ -7,5 +8,8 @@ def home(request):
     context = {'posts':Post.objects.all()}
     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+
 def about(request):
-    return render(request, 'blog/about.html', {'title':'About'})
+     return render(request, 'blog/about.html', {'title':'About'})
